def9ba87b015c1b95ef26b26f4c9920d40ee1aea
TGRSA13 <tgmchun@gmail.com>
2024-07-22 18:00:09 +0000

    Adding CSS & Images

--- 0x00-Webpack/task_2/js/dashboard_main.js
+++ 0x00-Webpack/task_2/js/dashboard_main.js
@@ -1,7 +1,7 @@
 const $ = require( "jquery" );
 // Load the full build
 const _ = require("lodash");
-import "./body.css";
+import "../css/main.css";
 
 let count = 0;
 
@@ -15,6 +15,9 @@ const $button = $("<button>Click here to get started</button>").on(
   _.debounce( updateCounter, 500, { leading: true, trailing: false })
 );
 
+$('body').append("<div id='logo'></div>");
+$('body').append("<p>Holberton Dashboard</p>");
 $('body').append("<p>Dashboard data for the students</p>");
 $('body').append($button);
 $('body').append("<p id='count'></p>");
+$('body').append("<p>Copyright - Holberton School</p>");
